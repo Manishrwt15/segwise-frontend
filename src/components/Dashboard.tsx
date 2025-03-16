80a660a0f02bb3f29dbf78d0a81a17390b31009d
Manish Rawat <132916222+Manishrwt15@users.noreply.github.com>
2025-03-16 12:49:46 +0530

    Fixed TypeScript & Pagination Issues

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,17 +1,9 @@
-import { useState } from "react";
-import { mockData } from "../data/mockData";
 import Table from "./Table";
 
 const Dashboard = () => {
-  const [currentPage, setCurrentPage] = useState(1);
-  const rowsPerPage = 5;
-
-  const totalPages = Math.ceil(mockData.length / rowsPerPage);
-  const currentData = mockData.slice((currentPage - 1) * rowsPerPage, currentPage * rowsPerPage);
-
   return (
     <div className="p-6">
-      <Table data={currentData} />
+      <Table />  
     </div>
   );
 };
